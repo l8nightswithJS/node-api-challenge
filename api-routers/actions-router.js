928413993718ab33ac0e1e56193f3e755b8e3b6f
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-03-20 20:36:46 -0700

    added get for actions on specific ptoject

--- api-routers/actions-router.js
+++ api-routers/actions-router.js
@@ -3,4 +3,20 @@ const ActionsRouter = require('../data/helpers/actionModel.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const id = req.query.id;
+
+    ActionsRouter.get(id)
+        .then(actions => {
+            if(actions) {
+                res.status(200).json(actions); 
+            } else if(actions.id) {
+                res.status(200).json(actions);
+            } else res.status(400).json({ err: "id not found" })
+        })
+        .catch(err => {
+            res.status(500).json({ error: "entered id not found" });
+        });
+});
+
 module.exports = router;
--- api-routers/projects-router.js
+++ api-routers/projects-router.js
@@ -3,4 +3,25 @@ const ProjectsRouter = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const id = req.query.project_id;
+
+    ProjectsRouter.getProjectActions(id)
+        .then(projects => {
+            if(id[0] == !null) {
+                res.status(200).json({projects});
+            } else {
+                res.status(400).json({ err: "id not found" })
+            } 
+        })
+        .catch(err => {
+            res.status(500).json({ error: "entered id not found" });
+        });
+});
+
+
+
+
+
+
 module.exports = router;
