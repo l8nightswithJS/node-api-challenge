b7adb9d629a7581dc1bfa75b0c578426ddd79c63
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-03-20 21:05:56 -0700

    create projects done

--- api-routers/projects-router.js
+++ api-routers/projects-router.js
@@ -6,10 +6,22 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const id = req.query.project_id;
 
-    ProjectsRouter.getProjectActions(id)
+    ProjectsRouter.get()
         .then(projects => {
+            res.status(200).json({projects});
+        })
+        .catch(err => {
+            res.status(500).json({ error: "entered id not found" });
+        });
+});
+
+router.get('/actions', (req, res) => {
+    const id = req.query.project_id;
+
+    ProjectsRouter.getProjectActions(id)
+        .then(actions => {
             if(id[0] == !null) {
-                res.status(200).json({projects});
+                res.status(200).json({actions});
             } else {
                 res.status(400).json({ err: "id not found" })
             } 
@@ -18,6 +30,25 @@ router.get('/', (req, res) => {
             res.status(500).json({ error: "entered id not found" });
         });
 });
+
+router.post('/', (req, res) => {
+    const project = req.body;
+
+    ProjectsRouter.insert(project)
+        .then(project => {
+            if(project) {
+                res.status(200).json(project);
+            } else {
+                res.status(400).json({ err: "id not found" })
+            } 
+        })
+        .catch(err => {
+            res.status(500).json({ error: "entered id not found" });
+        });
+});
+
+
+
 
 
 
